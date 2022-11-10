ff27c9307ffdbfb4f0924602cc71346f55e1e5fe
fif <fif@DESKTOP-BFHUIKE>
2022-11-11 09:58:34 +1200

    Добавьте файлы проекта.

---
+++ oglilgrena/Program.cs
@@ -0,0 +1,21 @@
+﻿//Скрипников 17 вар 3.1
+var k = double.Parse(Console.ReadLine());
+
+double sum = 0;
+for (var i = -3; i < k; i++)
+{
+
+    if (i == 5) continue;
+
+    double mult = 1;
+    for (var n = i; n < 2 * k; n++)
+    {
+        if (n == 2 || n == -4) continue;
+
+        mult *= (Math.Pow(n, 3) - 8) / (n + 4);
+    }
+
+    sum += Math.Pow(-1, i) / Math.Pow(i - 5, 2);
+}
+
+Console.WriteLine($"W = {sum}");
